d100d3bbe0638c6501c25a0b8c2d6429e62ccc89
Jasper <jaspershemilt@virginmedia.co.uk>
2013-07-24 02:52:16 +0100

    fixed libjpeg missing object for ios

--- platforms/ios/build_framework.py
+++ platforms/ios/build_framework.py
@@ -29,13 +29,6 @@ import glob, re, os, os.path, shutil, string, sys
 
 def build_opencv(srcroot, buildroot, target, arch):
     "builds OpenCV for device or simulator"
-	# copy over ~/Developer/openCV_Dev/opencv/platforms/ios/Info.plist.in  so put_framework_together is srcroot independent
-    # os.system("echo \"mkdir -p %s \" " % (buildroot))
-    # os.system("echo \"cp %s %s \" " % (os.path.join(srcroot,"platforms/ios/Info.plist.in"), buildroot))
-    # os.system("echo \"cp %s %s \" " % (os.path.join(srcroot,"platforms/ios/make_framework.py "), os.path.join(buildroot,"..")))
-    os.system("mkdir -p %s" % (buildroot))
-    os.system("cp %s %s" % (os.path.join(srcroot,"platforms/ios/Info.plist.in"), buildroot))
-    # os.system("cp %s %s" % (os.path.join(srcroot,"platforms/ios/make_framework.py "), os.path.join(buildroot,"..")))
 
     builddir = os.path.join(buildroot, target + '-' + arch)
     if not os.path.isdir(builddir):
@@ -43,16 +36,11 @@ def build_opencv(srcroot, buildroot, target, arch):
     currdir = os.getcwd()
     os.chdir(builddir)
     # for some reason, if you do not specify CMAKE_BUILD_TYPE, it puts libs to "RELEASE" rather than "Release"
-    # "-D CMAKE_BUILD_TYPE=Release " +
-    if target=="OSX" : cmakeargs = ("-G Xcode " +
-                     "-D CMAKE_BUILD_TYPE=Debug " +
-                     "-D BUILD_opencv_world=ON " +
-                     "-D CMAKE_INSTALL_PREFIX=install")
-    else: cmakeargs = ("-G Xcode " +
-                     "-D CMAKE_BUILD_TYPE=Debug " +
-                     "-D CMAKE_TOOLCHAIN_FILE=%s/platforms/ios/cmake/Toolchains/Toolchain-%s_Xcode.cmake " +
-                     "-D BUILD_opencv_world=ON " +
-                     "-D CMAKE_INSTALL_PREFIX=install") % (srcroot, target)
+    cmakeargs = ("-GXcode " +
+                "-DCMAKE_BUILD_TYPE=Release " +
+                "-DCMAKE_TOOLCHAIN_FILE=%s/platforms/ios/cmake/Toolchains/Toolchain-%s_Xcode.cmake " +
+                "-DBUILD_opencv_world=ON " +
+                "-DCMAKE_INSTALL_PREFIX=install") % (srcroot, target)
     # if cmake cache exists, just rerun cmake to update OpenCV.xproj if necessary
     if os.path.isfile(os.path.join(builddir, "CMakeCache.txt")):
         os.system("cmake %s ." % (cmakeargs,))
@@ -64,14 +52,58 @@ def build_opencv(srcroot, buildroot, target, arch):
         if os.path.isfile(wlib):
             os.remove(wlib)
 
-    if target=="OSX" : 
-    	os.system("xcodebuild -parallelizeTargets ARCHS=%s -jobs 8 -configuration Release -target ALL_BUILD" % (arch))
-    	os.system("xcodebuild ARCHS=%s -configuration Release -target install install" % (arch))
-    else:
-    	os.system("xcodebuild -parallelizeTargets ARCHS=%s -jobs 8 -sdk %s -configuration Debug -target ALL_BUILD" % (arch, target.lower()))
-    	os.system("xcodebuild ARCHS=%s -sdk %s -configuration Debug -target install install" % (arch, target.lower()))
-    
+    os.system("xcodebuild -parallelizeTargets ARCHS=%s -jobs 8 -sdk %s -configuration Release -target ALL_BUILD" % (arch, target.lower()))
+    os.system("xcodebuild ARCHS=%s -sdk %s -configuration Release -target install install" % (arch, target.lower()))
     os.chdir(currdir)
+
+def put_framework_together(srcroot, dstroot):
+    "constructs the framework directory after all the targets are built"
+
+    # find the list of targets (basically, ["iPhoneOS", "iPhoneSimulator"])
+    targetlist = glob.glob(os.path.join(dstroot, "build", "*"))
+    targetlist = [os.path.basename(t) for t in targetlist]
+
+    # set the current dir to the dst root
+    currdir = os.getcwd()
+    framework_dir = dstroot + "/opencv2.framework"
+    if os.path.isdir(framework_dir):
+        shutil.rmtree(framework_dir)
+    os.makedirs(framework_dir)
+    os.chdir(framework_dir)
+
+    # determine OpenCV version (without subminor part)
+    tdir0 = "../build/" + targetlist[0]
+    cfg = open(tdir0 + "/cvconfig.h", "rt")
+    for l in cfg.readlines():
+        if l.startswith("#define  VERSION"):
+            opencv_version = l[l.find("\"")+1:l.rfind(".")]
+            break
+    cfg.close()
+
+    # form the directory tree
+    dstdir = "Versions/A"
+    os.makedirs(dstdir + "/Resources")
+
+    # copy headers
+    shutil.copytree(tdir0 + "/install/include/opencv2", dstdir + "/Headers")
+
+    # make universal static lib
+    wlist = " ".join(["../build/" + t + "/lib/Release/libopencv_world.a" for t in targetlist])
+    os.system("lipo -create " + wlist + " -o " + dstdir + "/opencv2")
+
+    # form Info.plist
+    srcfile = open(srcroot + "/platforms/ios/Info.plist.in", "rt")
+    dstfile = open(dstdir + "/Resources/Info.plist", "wt")
+    for l in srcfile.readlines():
+        dstfile.write(l.replace("${VERSION}", opencv_version))
+    srcfile.close()
+    dstfile.close()
+
+    # make symbolic links
+    os.symlink("A", "Versions/Current")
+    os.symlink("Versions/Current/Headers", "Headers")
+    os.symlink("Versions/Current/Resources", "Resources")
+    os.symlink("Versions/Current/opencv2", "opencv2")
 
 
 def build_framework(srcroot, dstroot):
@@ -79,20 +111,10 @@ def build_framework(srcroot, dstroot):
 
     targets = ["iPhoneOS", "iPhoneOS", "iPhoneSimulator"]
     archs = ["armv7", "armv7s", "i386"]
-  #  targets = ["iPhoneSimulator"]
-  #  archs = ["i386"]
-  #  targets = ["OSX"]
-  #  archs = ["x86_64"]
     for i in range(len(targets)):
         build_opencv(srcroot, os.path.join(dstroot, "build"), targets[i], archs[i])
-    
-#    put_framework_together(srcroot, dstroot)
-    os.system("cp %s %s" % (os.path.join(srcroot,"platform/ios/make_framework.py "), dstroot))
-    os.system("echo \"cp %s %s \" " % (os.path.join(srcroot,"platform/ios/make_framework.py "), dstroot))
-    os.chdir(dstroot)
-    os.system("echo \"*****  PWD  *******\" " )
-    os.system("pwd")
-    import make_framework.py
+
+    put_framework_together(srcroot, dstroot)
 
 
 if __name__ == "__main__":
