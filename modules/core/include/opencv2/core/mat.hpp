21a231bb425a2a3825cb1a144c6198c155f69323
Jasper <jaspershemilt@virginmedia.co.uk>
2013-09-19 17:06:02 +0100

    Mat enum replication of type data fixed

--- modules/core/include/opencv2/core/mat.hpp
+++ modules/core/include/opencv2/core/mat.hpp
@@ -727,7 +727,10 @@ public:
     template<typename _Tp> MatConstIterator_<_Tp> end() const;
 
     enum { MAGIC_VAL  = 0x42FF0000, AUTO_STEP = 0, CONTINUOUS_FLAG = CV_MAT_CONT_FLAG, SUBMATRIX_FLAG = CV_SUBMAT_FLAG };
-    enum { MAGIC_MASK = 0xFFFF0000, TYPE_MASK = 0x00000FFF, DEPTH_MASK = 7 };
+    enum { MAGIC_MASK = CV_MAGIC_MASK, // 0xFFFF0000,
+        TYPE_MASK = CV_MAT_TYPE_MASK, // 0x00000FFF,
+        DEPTH_MASK = CV_MAT_DEPTH_MASK // 7
+    };
 
     /*! includes several bit-fields:
          - the magic signature
